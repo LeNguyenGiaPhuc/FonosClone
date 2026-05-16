f951695d2b9bd48e2d7ef9feabb240ee6d6df229
ponpon2k5 <phongthanh2k55@gmail.com>
2026-05-16 21:29:25 +0700

    add gui challenges, home, library

---
+++ app/src/main/java/hcmute/com/fonosclone/ChallengesActivity.java
@@ -0,0 +1,12 @@
+package hcmute.com.fonosclone;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChallengesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenges);
+    }
+}
